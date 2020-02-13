ed2ab60cfee58e6c01477177a42b7618d44dcbb0
Augustin <augustin.peyridieux@saagie.com>
2020-02-13 14:40:08 +0100

    updating test.py

--- pySaagie_connect/tests/test.py
+++ pySaagie_connect/tests/test.py
@@ -1,10 +1,14 @@
-import pySaagie_connect as sc
+from pySaagie_connect import pySaagie_connect as sc
 
-sc.return_client_hdfs('username')
+client_hdfs = sc.return_client_hdfs(user='auser_name'
+                                    , list_name_nodes=['http://nn1', 'http://nn2']
+                                    , port_hdfs=50070)
+print(client_hdfs.status('/'))
 
-sc.return_ibis_client('username'
+client_impala = sc.return_ibis_client('user_name'
                       , 'password'
                       , ['dn1', 'dn2', 'dn3', 'dn4']
-                      , ['nn1', 'nn2']
+                      , ['http://nn1', 'http://nn2']
                       , 21050
                       , 50070)
+print(client_impala.exists_database('default'))
